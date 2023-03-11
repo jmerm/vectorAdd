356bca1aaeabe6cbda57fd6201a5debaa21226f1
merm <johnm.merm@gmail.com>
2023-03-10 21:35:45 -0700

    Add CMakeLists

---
+++ build/CMakeFiles/vectAdd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectAdd.dir/vectAdd.cu.o"
+  "CMakeFiles/vectAdd.dir/cmake_device_link.o"
+  "vectAdd.pdb"
+  "vectAdd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/vectAdd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
